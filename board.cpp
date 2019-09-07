33b0dadde914d79dafcae8fde3a05d89d48b5719
tkg97 <gupta.gupta.tarun@gmail.com>
2019-09-07 18:44:35 +0530

    better coding notations for member variables

--- board.cpp
+++ board.cpp
@@ -21,7 +21,7 @@ Board::Board(std::string filePath)
             int low, high;
             file >> high >> low;
             Snake_Ladder *s = new Snake(low, high);
-            components.push_back(s);
+            this->components.push_back(s);
         }
         file >> numLadders;
         for (int i = 0; i < numLadders; i++)
@@ -29,7 +29,7 @@ Board::Board(std::string filePath)
             int low, high;
             file >> low >> high;
             Snake_Ladder *s = new Ladder(low, high);
-            components.push_back(s);
+            this->components.push_back(s);
         }
         file >> numPlayers;
         for (int i = 0; i < numPlayers; i++)
@@ -37,24 +37,24 @@ Board::Board(std::string filePath)
             std::string name;
             file >> name;
             GamePlayer *p = new GamePlayer(name);
-            players.push_back(p);
+            this->players.push_back(p);
         }
     }
 }
 
 void Board::startGame()
 {
-    int activePlayers = players.size();
+    int activePlayers = this->players.size();
     while (activePlayers > 1)
     {
-        for (int i = 0; i < players.size(); i++)
+        for (int i = 0; i < this->players.size(); i++)
         {
-            if (!players[i]->getActiveStatus())
+            if (!this->players[i]->getActiveStatus())
                 continue;
 
-            int k = players[i]->rollDice();
-            int pos_init = players[i]->getCurrPosition();
-            for (Snake_Ladder *comp : components)
+            int k = this->players[i]->rollDice();
+            int pos_init = this->players[i]->getCurrPosition();
+            for (Snake_Ladder *comp : this->components)
             {
                 int modified_pos = comp->modifyPosition(pos_init + k);
                 if (modified_pos > this->size)
@@ -64,22 +64,22 @@ void Board::startGame()
                     // Thus we don't need to do any update in the current postion of the player
                     break;
                 }
-                players[i]->updatePosition(modified_pos);
+                this->players[i]->updatePosition(modified_pos);
                 if (modified_pos != (pos_init + k))
                 {
                     break;
                 }
             }
-            int pos_final = players[i]->getCurrPosition();
+            int pos_final = this->players[i]->getCurrPosition();
 
-            std::cout << players[i]->getName() << " rolled a " << k << " and moved from " << pos_init << " to " << pos_final << std::endl;
+            std::cout << this->players[i]->getName() << " rolled a " << k << " and moved from " << pos_init << " to " << pos_final << std::endl;
 
             if (pos_final == this->size)
             {
                 std::cout << "-------------------------------------------------------" << std::endl;
-                std::cout << players[i]->getName() << " wins among the currently active players" << std::endl;
+                std::cout << this->players[i]->getName() << " wins among the currently active players" << std::endl;
                 std::cout << "-------------------------------------------------------" << std::endl;
-                players[i]->setActiveStatus(false);
+                this->players[i]->setActiveStatus(false);
                 activePlayers--;
                 if (activePlayers <= 1)
                     break;
@@ -90,7 +90,7 @@ void Board::startGame()
 
 void Board::restartGame()
 {
-    for (GamePlayer *p : players)
+    for (GamePlayer *p : this->players)
     {
         p->updatePosition(0);
         p->setActiveStatus(true);
