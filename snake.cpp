5e0ea858a59ff28b3462a38ee27233413802e56a
ricskr <sutirtharej@gmail.com>
2019-09-07 18:14:48 +0530

    class added

--- snake.cpp
+++ snake.cpp
@@ -11,82 +11,86 @@ unordered_map<int, int>snakeT;
 unordered_map<int, int>ladderH;
 unordered_map<int, int>ladderT;
 
-void take_input_for_snakes(){
-	int number_of_snake; cin >> number_of_snake;
+class games{
+	public :
+	void take_input_for_snakes(){
+		int number_of_snake; cin >> number_of_snake;
 
-	///intaking all the snake head and tail
+		///intaking all the snake head and tail
 
-	for(int i=0; i<number_of_snake; i++){
-		int head ,tail; cin >> head >> tail;
-		snakeH[ head ] = tail;
-		snakeT[ tail ] = head;
-	}
-}
-
-void take_input_for_ladders(){
-	int num_of_lad; cin >> num_of_lad;
-
-	// intaking all the ladders
-
-	for(int i=0; i<num_of_lad; i++){
-		int head, tail; cin >> head >> tail;
-		ladderH[ head ] =  tail;
-		ladderT[ tail ] = head;
+		for(int i=0; i<number_of_snake; i++){
+			int head ,tail; cin >> head >> tail;
+			snakeH[ head ] = tail;
+			snakeT[ tail ] = head;
+		}
 	}
 
-}
-void intake_player(){
-	int n; cin >> n;
-	vector<pair<string, int> > players;
-	for(int i=0; i<n; i++) {
-		string s; cin >> s;
-		players.pb( mp( s, 0 ) );
-	}
-}
-void play(){
-	int khiladi_number = 0;
-	while(1){
-		
-		khiladi_number %= n;
-		
-		string s = players[ khiladi_number ].first;
-		
-		int cur_start = players[ khiladi_number ].second;
-		
-		int move = rand() % 7;
-		
-		if(move + cur_start <= 100)
-			int end = cur_start + move;
-		
-		cout<< s << " rolled a " << move <<" from "
-			<<cur_start<<" to "<<end;
-		
-		if(snakeH.find( end ) != snakeH.end()  ){
-			end = snakeH[ end ];
-		}
-		else if(snakeT.find( end ) != snakeT.end()  ){
-			end = snakeT[ end ];
-		}
-		else if(ladderH.find( end ) != ladderH.end()  ){
-			end = ladderH[ end ];
-		}
-		else if(ladderT.find( end ) != ladderT.end()  ){
-			end = ladderT[ end ];
-		}
-		++khiladi_number;
+	void take_input_for_ladders(){
+		int num_of_lad; cin >> num_of_lad;
 
-		if(end == 100){
-			cout<< s << " wins the game";
-			break;
+		// intaking all the ladders
+
+		for(int i=0; i<num_of_lad; i++){
+			int head, tail; cin >> head >> tail;
+			ladderH[ head ] =  tail;
+			ladderT[ tail ] = head;
+		}
+
+	}
+	void intake_player(){
+		int n; cin >> n;
+		vector<pair<string, int> > players;
+		for(int i=0; i<n; i++) {
+			string s; cin >> s;
+			players.pb( mp( s, 0 ) );
+		}
+	}
+	void play(){
+		int khiladi_number = 0;
+		while(1){
+			
+			khiladi_number %= n;
+			
+			string s = players[ khiladi_number ].first;
+			
+			int cur_start = players[ khiladi_number ].second;
+			
+			int move = rand() % 7;
+			
+			if(move + cur_start <= 100)
+				int end = cur_start + move;
+			
+			cout<< s << " rolled a " << move <<" from "
+				<<cur_start<<" to "<<end;
+			
+			if(snakeH.find( end ) != snakeH.end()  ){
+				end = snakeH[ end ];
+			}
+			else if(snakeT.find( end ) != snakeT.end()  ){
+				end = snakeT[ end ];
+			}
+			else if(ladderH.find( end ) != ladderH.end()  ){
+				end = ladderH[ end ];
+			}
+			else if(ladderT.find( end ) != ladderT.end()  ){
+				end = ladderT[ end ];
+			}
+			++khiladi_number;
+
+			if(end == 100){
+				cout<< s << " wins the game";
+				break;
+			}
 		}
 	}
 }
 int32_t main()
 {
 	//fast
-	take_input_for_snakes();
-	take_input_for_ladders();
-	intake_player();
-	play();
+	games ob;
+	ob.take_input_for_snakes();
+	ob.take_input_for_ladders();
+	ob.intake_player();
+	ob.play();
 	return 0;
 }
