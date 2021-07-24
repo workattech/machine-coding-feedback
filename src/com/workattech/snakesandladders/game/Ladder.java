b3e50a684569867dfcbeac7bd99f829d868eed47
PrafulAnand <prafulanand724@gmail.com>
2021-07-24 19:28:59 +0530

    ladder class added with constructor and getters and setters

--- src/com/workattech/snakesandladders/game/Ladder.java
+++ src/com/workattech/snakesandladders/game/Ladder.java
@@ -1,4 +1,20 @@
 package com.workattech.snakesandladders.game;
 
 public class Ladder {
+
+    private int ladderHead;
+    private int ladderSnake;
+
+    public Ladder(int ladderHead, int ladderSnake) {
+        this.ladderHead = ladderHead;
+        this.ladderSnake = ladderSnake;
+    }
+
+    public int getLadderHead() {
+        return ladderHead;
+    }
+
+    public int getLadderSnake() {
+        return ladderSnake;
+    }
 }
