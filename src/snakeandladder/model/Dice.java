7a1ba7ddaa188c84ea9ea38d88e8542e6447a06e
Anmol Joshi <anmol01joshi@gmail.com>
2021-09-08 13:15:59 +0530

    simplify dice

--- src/snakeandladder/model/Dice.java
+++ src/snakeandladder/model/Dice.java
@@ -2,6 +2,6 @@ package snakeandladder.model;
 
 public class Dice {
     public int getDiceRoll(){
-        return (int)(Math.random()*(7-1)+1);//used to get random number in range[1,6]
+        return (int)(Math.random()*(6)+1);//used to get random number in range[1,6]
     }
 }
