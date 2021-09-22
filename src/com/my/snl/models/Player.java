a6650ab68b932d2b6b4aa955396830dc26dfa903
ravitejabonu <raviteja4101@gmail.com>
2021-09-22 17:30:48 +0530

    solved the problem

---
+++ src/com/my/snl/models/Player.java
@@ -0,0 +1,21 @@
+package com.my.snl.models;
+
+import java.util.UUID;
+
+public class Player {
+    private String name;
+    private String id;
+
+    public Player(String name) {
+        this.name = name;
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
