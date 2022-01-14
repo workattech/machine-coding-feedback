c69804758a46573f6710deda24e5bbf1fae5c588
Sahil Yadav <sahilyadavnikum@gmail.com>
2022-01-15 03:13:20 +0530

    First java program

---
+++ tic-tac/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Main {
+    private Integer a;
+    private List<Integer> arr;
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
+    public List<Integer> getArr() {
+        return arr;
+    }
+
+    public void setArr(List<Integer> arr) {
+        this.arr = arr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("first java program");
+    }
+}
