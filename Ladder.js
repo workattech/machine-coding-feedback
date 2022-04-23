1729093d991e909ea5ff2df9be1a0c6a7d44c281
Ayush Tiwari <62030895+ayush2323@users.noreply.github.com>
2022-04-23 18:30:02 +0530

    Add files via upload

---
+++ Ladder.js
@@ -0,0 +1,34 @@
+export const LadderPositions = [
+ {
+    start: 2,
+    end: 37
+  },
+  {
+    start: 27,
+    end: 46
+  },
+  {
+    start: 10,
+    end: 32
+  },
+  {
+    start: 51,
+    end: 68
+  },
+  {
+    start: 61,
+    end: 79
+  },
+  {
+    start: 65,
+    end: 84
+  },
+  {
+    start: 71,
+    end: 91
+  },
+  {
+    start: 81,
+    end: 100
+  }
+];
---
+++ Snake.js
@@ -0,0 +1,37 @@
+export const SnakePositions = [
+  {
+    start: 62,
+    end: 5
+  },
+  {
+    start: 33,
+    end: 6
+  },
+  {
+    start: 49,
+    end: 9
+  },
+  {
+    start: 88,
+    end: 16
+  },
+  {
+    start: 41,
+    end: 20
+  },
+  {
+    start: 56,
+    end: 53
+  },
+  {
+    start: 98,
+    end: 64
+  },
+  {
+    start: 93,
+    end: 73
+  },{
+    start: 95,
+    end: 75
+  }
+];
