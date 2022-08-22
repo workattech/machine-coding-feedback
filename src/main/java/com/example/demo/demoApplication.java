97233fdc2a011bf565696bbce95d67ede5518c7e
Mayank Bandil <mayank.nitb11@gmail.com>
2022-08-22 23:35:03 +0530

    add

---
+++ src/main/java/com/example/demo/demoApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class demoApplication {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
--- src/main/java/com/example/snakeLadder/constants/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.example.snakeLadder.constants;
-
-public class Constants {
-    public static final int DEFAULT_NO_OF_DICES = 2;
-}
