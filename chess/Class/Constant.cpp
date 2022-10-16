46a97a2cac96d37d738fbcf0c057862ce21f0cda
abhishek <malviya.abhishek853@gmail.com>
2022-10-17 02:04:43 +0530

    chess

---
+++ chess/Class/Constant.cpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+string BLACK = "B";
+string WHITE = "W";
+
+string P = "P";
+string R = "R";
+string N = "N";
+string B = "B";
+string Q = "Q";
+string K = "K";
---
+++ chess/Readme.md
@@ -0,0 +1,5 @@
+g++ *.cpp -o main; ./main;
+
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
