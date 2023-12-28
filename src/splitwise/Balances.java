3454af1c2f39d2075aa54c1ba46812316cecb0fa
Vedang Karwa <kvedang12@gmail.com>
2023-12-28 22:49:50 +0000

    Cleaned master branch

--- src/splitwise/Balances.java
+++
@@ -1,9 +0,0 @@
-package src.splitwise;
-
-import java.util.List;
-
-public interface Balances {
-    public static final AppConfig appConfig = AppConfig.getInstance();
-
-    public List<String> getBalances() throws IllegalArgumentException;
-}
--- src/splitwise/DoubleHelper.java
+++
@@ -1,7 +0,0 @@
-package src.splitwise;
-
-public class DoubleHelper {
-    double getTwoDigitRoundedAmount(double amount) {
-        return Math.round(amount * 100.0) / 100.0;
-    }
-}
